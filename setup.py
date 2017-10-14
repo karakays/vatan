4ab10838b8b08bcfb8bccac8e4628d5693ebab6b
Selcuk Karakayali <selcuk@eftsoftware.com>
2017-10-14 22:18:16 +0300

    add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8
+from distutils.core import setup
+
+setup(
+    name='vatan',
+    version='0.1',
+    author='Selçuk Karakayalı',
+    author_email='skarakyali@gmail.com',
+    packages=['vatan'],
+    licence='GNU GENERAL PUBLIC LICENSE',
+    # long_description=open('README.txt').read(),
+)
