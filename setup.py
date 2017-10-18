4bd03a8958a70a615021f6a215d03802e1cf6f21
Selcuk Karakayali <selcuk@eftsoftware.com>
2017-10-18 16:03:49 +0300

    entry point

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='vatan',
     version='0.1',
     author='Selçuk Karakayalı',
-    author_email='skarakyali@gmail.com',
-    packages=['vatan'],
-    licence='GNU GENERAL PUBLIC LICENSE',
-    # long_description=open('README.txt').read(),
+    author_email='skarakayali@gmail.com',
+    url='http://github.com/karakays/vatan/',
+    packages=find_packages(),
+    license='GNU GENERAL PUBLIC LICENSE',
+    entry_points={
+        'console_scripts': [
+            'vatan = vatan.__main__:main'
+            ]
+    }
 )
